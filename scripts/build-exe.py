cc760a2971bea3536099635bf45d0a60157bd931
Bivas Kumar <thetrotfreak@yahoo.com>
2024-01-12 22:22:25 +0530

    Update build-exe.py
    Lowercase the app python script name

--- scripts/build-exe.py
+++ scripts/build-exe.py
@@ -3,26 +3,28 @@ import shutil
 
 import PyInstaller.__main__
 
-if __name__ == '__main__':
-    APP_NAME = r'WS2DB'
+if __name__ == "__main__":
+    APP_NAME = r"WS2DB"
     APP_DIR = pathlib.PureWindowsPath(__file__).parents[1]
-    APP_PATH = APP_DIR.joinpath(f'{APP_NAME}', f'{APP_NAME}.py')
-    ICON_PATH = APP_DIR.joinpath(r'icons', 'spotlight.ico')
-    DIST_DIR = pathlib.WindowsPath(APP_DIR.joinpath(r'release'))
+    APP_PATH = APP_DIR.joinpath(f"{APP_NAME}", f"{APP_NAME}.py".lower())
+    ICON_PATH = APP_DIR.joinpath(r"icons", "spotlight.ico")
+    DIST_DIR = pathlib.WindowsPath(APP_DIR.joinpath(r"release"))
     try:
         DIST_DIR.mkdir(exist_ok=True)
-        PyInstaller.__main__.run([
-            fr'{APP_PATH}',
-            '-i',
-            fr'{ICON_PATH}',
-            '-n',
-            fr'{APP_NAME}',
-            '--distpath',
-            fr'{DIST_DIR}',
-            '--clean',
-            '--noconsole',
-            '--noconfirm'
-        ])
-        shutil.make_archive(fr'{APP_NAME}', 'zip', root_dir=fr'{DIST_DIR}')
+        PyInstaller.__main__.run(
+            [
+                rf"{APP_PATH}",
+                "-i",
+                rf"{ICON_PATH}",
+                "-n",
+                rf"{APP_NAME}",
+                "--distpath",
+                rf"{DIST_DIR}",
+                "--clean",
+                "--noconsole",
+                "--noconfirm",
+            ]
+        )
+        shutil.make_archive(rf"{APP_NAME}", "zip", root_dir=rf"{DIST_DIR}")
     except FileExistsError as e:
-        print('FAILED', e)
+        print("FAILED", e)
